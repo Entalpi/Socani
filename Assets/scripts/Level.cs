d8b6f2a0f0e003c5dd936b2abc5e5755881a2239
Lingtorp <alexander@lingtorp.com>
2017-10-28 09:02:01 +0200

    Added time to the level

--- Assets/scripts/Level.cs
+++ Assets/scripts/Level.cs
@@ -4,7 +4,11 @@ using UnityEngine;
 
 [System.Serializable]
 public class Level : MonoBehaviour {
+    // # of seconds to complete the level
+    public float time;
+    // # of coins on the level 
     public int num_coins = 0;
+
 	public Texture2D[] tile_layers;
 
 	public Dictionary<Vector2Int, List<GameObject>> load(GameBoard game_board) {
