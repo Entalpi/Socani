a6f4b40c1f1a1838816d3ba7f52f6272d63eea77
Lingtorp <alexander@lingtorp.com>
2017-10-30 12:31:34 +0100

    Coin now plays a sound when picked up

--- Assets/scripts/Coin.cs
+++ Assets/scripts/Coin.cs
@@ -3,12 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Coin : MonoBehaviour {
-    private void OnCollisionEnter2D(Collision2D collision) {
+    private void OnCollisionEnter2D(Collision2D col) {
         GameBoard board = FindObjectOfType<GameBoard>();
         if (board) {
             var position = board.transform_to_board_position(transform.position);
             if (board.remove_from_board(gameObject, position)) {
                 // Play effects
+                if (col.collider.tag == "Player") {
+                    AudioManager.instance.Play("coin-pickup");
+                } else {
+                    
+                }
                 Destroy(gameObject);
             }
         }
