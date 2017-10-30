4d736663f216bdb7f68b25d2491317225ddeba74
Lingtorp <alexander@lingtorp.com>
2017-10-30 12:30:43 +0100

    Created Sound class

---
+++ Assets/scripts/Sound.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+
+[System.Serializable]
+public class Sound {
+    public bool enabled = true;
+    public bool loop = false;
+
+    public AudioClip clip;
+
+    public string name;
+
+    [Range(0f, 1f)]
+    public float volume = 0.5f;
+
+    [Range(0.1f, 3f)]
+    public float pitch = 1.0f;
+
+    [HideInInspector]
+    public AudioSource source;
+}
+
