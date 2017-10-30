c1ceb93b2d16c56893817128e8756317781d0289
Lingtorp <alexander@lingtorp.com>
2017-10-30 12:31:21 +0100

    Added AudioManager singleton prefab/script/object

---
+++ Assets/scripts/AudioManager.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioManager : MonoBehaviour {
+
+    public static AudioManager instance;
+
+    // Disable/enable all the sounds
+    public bool on;
+
+    public Sound[] sounds;
+
+	// Use this for initialization
+	void Awake () {
+        if (instance == null) {
+            instance = this;
+        } else {
+            Destroy(gameObject);
+            return;
+        }
+
+        DontDestroyOnLoad(this);
+
+		foreach (Sound s in sounds) {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
+        }
+	}
+
+    void Start () {
+        Play("theme-song");
+    }
+
+    public Sound GetSound(string name) {
+        return Array.Find(sounds, sound => sound.name == name);
+    }
+
+    public void Play(Sound s) {
+        if (s != null) {
+            s.source.Play();
+        }
+    }
+
+    public void Play(string name) {
+        if (!on) {
+            return;
+        }
+
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if (s == null) {
+            Debug.LogWarning("Could not find sound with name: " + name);
+            return;
+        }
+        s.source.Play();
+    }
+}
