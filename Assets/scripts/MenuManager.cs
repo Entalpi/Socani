ee30ea034d8dd92acbadbcddb7d46f2f95f0b52d
Lingtorp <alexander@lingtorp.com>
2017-10-31 10:01:29 +0100

    Added start menu scene

---
+++ Assets/scripts/MenuManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour {
+
+    public GameObject musicButton;
+    public GameObject soundButton;
+
+    // TODO: Load level selection scene
+    public void Play() {
+        SceneManager.LoadScene("player");
+    } 
+
+    public void ToggleMusic() {
+        AudioManager.instance.music = !AudioManager.instance.music;
+        // TODO: Change icon on button and so on
+        if (AudioManager.instance.music) {
+
+        } else {
+
+        }
+    }
+
+    public void ToggleSoundEffects() {
+        AudioManager.instance.effect = !AudioManager.instance.effect;
+        // TODO: Change icon on button and so on
+        if (AudioManager.instance.effect) {
+
+        } else {
+
+        }
+    }
+}
