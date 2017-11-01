8704cd83c3f3124ee5944e8577b0a83ab6288073
Lingtorp <alexander@lingtorp.com>
2017-11-01 21:44:18 +0100

    Level selection menu is now dynamically populated with buttons!

--- Assets/scripts/LevelSelector.cs
+++ Assets/scripts/LevelSelector.cs
@@ -6,15 +6,23 @@ using UnityEngine.UI;
 public class LevelSelector : MonoBehaviour {
 
     public GameObject scrollView;
-    public Button[] buttons;
-    public Button buttonPrefab;
+    public GameObject buttonPrefab;
 
 	// Use this for initialization
 	void Start () {
         Level[] levels = LevelManager.instance.levels;
-        buttons = new Button[levels.Length];
-        foreach (Level level in levels) {
-
+        for (int i = 0; i < levels.Length; i++) {
+            GameObject obj = Instantiate(buttonPrefab);
+            Button btn = obj.GetComponent<Button>();
+            if (btn) {
+                int idx = i; // Using only 'i' does not work (C# lambda uses the variable value at i AFTER the loop is done)
+                btn.onClick.AddListener(() => onButtonClicked(levels[idx]));
+                btn.transform.SetParent(scrollView.transform, false);
+            }
         }
 	}
+
+    private void onButtonClicked(Level level) {
+        Debug.Log("Button clicked with Level: " + level.levelIndex);
+    }
 }
