8054990e205788da9f87becb486418107dacd698
Lingtorp <alexander@lingtorp.com>
2017-11-14 19:56:17 +0100

    Removed animations

---
+++ Assets/Gradient.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gradient : MonoBehaviour {
+    public Color startColor = Color.white;
+    public Color endColor   = Color.black;
+
+	void Start () {
+        MeshFilter meshFilter = GetComponent<MeshFilter>();
+        if (meshFilter) {
+            Debug.Log(meshFilter.mesh.vertices.Length);
+            Debug.Log(meshFilter.mesh.colors.Length);
+            meshFilter.mesh.colors = new Color[] { startColor, startColor, endColor, endColor };
+        }
+	}
+}
