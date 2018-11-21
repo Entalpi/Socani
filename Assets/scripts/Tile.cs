f91fc42a7e04fcf8427739a5a042cb69a6e21f15
Alexander Lingtorp <Entalpi@users.noreply.github.com>
2018-11-21 10:02:01 +0100

    Update Tile.cs

--- Assets/scripts/Tile.cs
+++ Assets/scripts/Tile.cs
@@ -8,15 +8,15 @@ public class Tile : MonoBehaviour {
 	public float inverseMoveTime = 1.0f / 0.1f;
 
 	void Start() {
-		// Figure out initial board position
-		GameBoard board = FindObjectOfType<GameBoard> ();
-		board_position = board.transform_to_board_position (transform.position);
+    // Figure out initial board position
+    GameBoard board = FindObjectOfType<GameBoard>();
+		board_position = board.transform_to_board_position(transform.position);
 	}
 
 	public IEnumerator smoothMovement(Vector3 end) {
 		float sqrRemainingDistance = (transform.position - end).sqrMagnitude;
 		while (sqrRemainingDistance > float.Epsilon) {
-			Vector3 newPosition = Vector3.MoveTowards (transform.position, end, inverseMoveTime * Time.deltaTime);
+			Vector3 newPosition = Vector3.MoveTowards(transform.position, end, inverseMoveTime * Time.deltaTime);
 			transform.position = newPosition;
 			sqrRemainingDistance = (transform.position - end).sqrMagnitude;
 			yield return null;
