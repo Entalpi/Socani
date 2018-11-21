a0292eeecfc3860ad63f1f6ead97fea11b771f8b
Alexander Lingtorp <Entalpi@users.noreply.github.com>
2018-11-21 10:02:15 +0100

    Update Level.cs

--- Assets/scripts/Level.cs
+++ Assets/scripts/Level.cs
@@ -17,12 +17,12 @@ public class Level : MonoBehaviour {
 
 	// Loads the tile layers in to a Dictionary with the tile position as key and a list of tiles as the value
 	public Dictionary<Vector2Int, List<GameObject>> load(GameBoard game_board) {
-		Dictionary<Vector2Int, List<GameObject>> board = new Dictionary<Vector2Int, List<GameObject>> ();
+		Dictionary<Vector2Int, List<GameObject>> board = new Dictionary<Vector2Int, List<GameObject>>();
 
     int maxWidth  = 0;
     int maxHeight = 0;
 
-		for (int z = 0; z < tile_layers.Length; z++) {
+    for (int z = 0; z < tile_layers.Length; z++) {
 			Texture2D tile_layer = tile_layers[z];
 
       if (tile_layer.width  > maxWidth)  { maxWidth = tile_layer.width; }
@@ -31,7 +31,7 @@ public class Level : MonoBehaviour {
       for (int x = 0; x < tile_layer.width; x++) {
 				for (int y = 0; y < tile_layer.height; y++) {
 					// Generate a new tile
-					Color color = tile_layers [z].GetPixel(x, y);
+					Color color = tile_layers[z].GetPixel(x, y);
 					if (color.a == 0.0f) { continue; }
 
 					var position = new Vector2Int(x, y);
