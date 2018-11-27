90045f6c28bcb913931b724375ed71fff61c53fa
Alexander Lingtorp <Entalpi@users.noreply.github.com>
2018-11-27 18:46:31 +0100

    Update Crate.cs

--- Assets/scripts/Crate.cs
+++ Assets/scripts/Crate.cs
@@ -3,5 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Crate : MonoBehaviour {
-	
+  private void OnCollisionEnter2D(Collision2D col) {
+    GameBoard board = FindObjectOfType<GameBoard>();
+    if (board) {
+      // Play effects
+      if (col.collider.tag == "Player") {
+        AudioManager.instance.Play("coin-pickup");
+      }
+     }
+  }
 }
