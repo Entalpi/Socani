5d64e23a837cd6f73212d2c3a248c5d059af95ea
Alexander Lingtorp <Entalpi@users.noreply.github.com>
2018-12-14 12:13:59 +0100

    Basic walking animation

--- Assets/scripts/Player.cs
+++ Assets/scripts/Player.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 	// Private
-	private const float move_cooldown = 0.25f;
+	private const float MOVE_COOLDOWN = 0.25f;
 	private float since_last_move = 0.0f;
 
 	// Player board tile representation
 	private Tile tile; 
 	private GameBoard board;
   private Rigidbody2D rb2D;
+  private Animator animator;
+
+  // Animation constants
+  private const int WALK_STATE_LEFT = 1;
+  private const int WALK_STATE_UP = 2;
+  private const int WALK_STATE_RIGHT = 3;
+  private const int WALK_STATE_DOWN = 4;
 
 	void Start() {
+    animator = GetComponent<Animator>();
     board = FindObjectOfType<GameBoard>();
 		tile = GetComponent<Tile>();
     // Disable rotation on the RB
@@ -26,25 +34,26 @@ public class Player : MonoBehaviour {
 	
 	void Update() {
 		since_last_move += Time.deltaTime;
-    if (since_last_move <= move_cooldown) { return; }
+    if (since_last_move <= MOVE_COOLDOWN) { return; }
 		bool did_move = false;
 		if (Input.GetKey("up")) {
       did_move = board.valid_move(gameObject, tile.boardPosition, Vector3Int.up);
+      animator.SetInteger("WalkingState", WALK_STATE_UP);
 		} else if (Input.GetKey("down")) {
       did_move = board.valid_move(gameObject, tile.boardPosition, Vector3Int.down);
+      animator.SetInteger("WalkingState", WALK_STATE_DOWN);
     }
     else if (Input.GetKey("left")) {
       did_move = board.valid_move(gameObject, tile.boardPosition, Vector3Int.left);
+      animator.SetInteger("WalkingState", WALK_STATE_LEFT);
     }
     else if (Input.GetKey("right")) {
       did_move = board.valid_move(gameObject, tile.boardPosition, Vector3Int.right);
+      animator.SetInteger("WalkingState", WALK_STATE_RIGHT);
     }
     if (did_move) {
 			board.endMove();
 			since_last_move = 0.0f;
-			if (board.checkGamestate()) {
-				Debug.Log("Victory!");
-			}
 		}
   }
 
