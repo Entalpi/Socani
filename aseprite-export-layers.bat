a6cda7e3dbc0a42c3ceb71d2610e5f644dd8ba5b
Alexander Lingtorp <Entalpi@users.noreply.github.com>
2019-03-19 11:51:37 +0100

    Added level 9

--- aseprite-export-layers.bat
+++ aseprite-export-layers.bat
@@ -2,7 +2,7 @@ D:
 cd \SteamLibrary\steamapps\common\Aseprite
 :: dir /b /o:gn
 @set ASEPRITE=Aseprite.exe
-@set INPUT_NAME=level8
-@set OUTPUT_NAME=level8
+@set INPUT_NAME=level9
+@set OUTPUT_NAME=level9
 %ASEPRITE% -b --split-layers %INPUT_NAME%.aseprite --save-as %OUTPUT_NAME%-{layer}.png
 timeout /t -1
