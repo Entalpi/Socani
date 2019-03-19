e7cf00a23fcf3353600a70c0ad1eb0caa06fcc9c
Alexander Lingtorp <Entalpi@users.noreply.github.com>
2019-03-19 21:39:57 +0100

    Create SocaniColors.cs

---
+++ Assets/scripts/SocaniColors.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+// Game defining color palette
+static class SocaniColor {
+  public static Color InformationText() {
+    return new Color(1.0f, 0.8896552f, 1.0f);
+  }
+
+  public static Color PositiveText() {
+    return new Color(0.1803922f, 0.8000001f, 0.4392157f);
+  }
+
+  // Neutral color 
+  public static Color ActionableText() {
+    return new Color(0.1803922f, 0.8000001f, 0.4392157f);
+  }
+
+  public static Color NegativeText() {
+    return new Color(0.1803922f, 0.8000001f, 0.4392157f);
+  }
+}
