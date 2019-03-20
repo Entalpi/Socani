7d2942ed312ebccdb3951a4ad9bb9d8d9528fb45
Alexander Lingtorp <Entalpi@users.noreply.github.com>
2019-03-20 06:52:26 +0100

    Update SocaniColors.cs

--- Assets/scripts/SocaniColors.cs
+++ Assets/scripts/SocaniColors.cs
@@ -2,20 +2,20 @@
 
 // Game defining color palette
 static class SocaniColor {
-  public static Color InformationText() {
-    return new Color(1.0f, 0.8896552f, 1.0f);
+  public static Color InformationText {
+    get { return new Color(1.0f, 0.8896552f, 1.0f); }
   }
 
-  public static Color PositiveText() {
-    return new Color(0.1803922f, 0.8000001f, 0.4392157f);
+  public static Color PositiveText {
+    get { return new Color(0.1803922f, 0.8000001f, 0.4392157f); }
   }
 
   // Neutral color 
-  public static Color ActionableText() {
-    return new Color(0.1803922f, 0.8000001f, 0.4392157f);
+  public static Color ActionableText {
+    get { return new Color(0.1803922f, 0.8000001f, 0.4392157f); }
   }
 
-  public static Color NegativeText() {
-    return new Color(0.1803922f, 0.8000001f, 0.4392157f);
+  public static Color NegativeText {
+    get { return new Color(0.1803922f, 0.8000001f, 0.4392157f); }
   }
 }
