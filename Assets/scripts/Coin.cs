5a8b148cca5a23f88abdb9e9cea05a7f8f592330
Alexander Lingtorp <Entalpi@users.noreply.github.com>
2019-03-23 08:17:32 +0100

    Update Coin.cs

--- Assets/scripts/Coin.cs
+++ Assets/scripts/Coin.cs
@@ -1,13 +1,13 @@
 ﻿using UnityEngine;
 
 public class Coin : MonoBehaviour {
-    private void OnCollisionEnter2D(Collision2D col) {
+  private void OnTriggerEnter2D(Collider2D col) {
         GameBoard board = FindObjectOfType<GameBoard>();
         if (board) {
             var position = board.world_to_board_position(transform.position);
             if (board.remove_from_board(gameObject, position)) {
                 // Play effects
-                if (col.collider.tag == "Player") {
+                if (col.gameObject.tag == "Player") {
                     AudioManager.instance.Play("coin-pickup");
                 } else {
                     AudioManager.instance.Play("coin-destroy");
