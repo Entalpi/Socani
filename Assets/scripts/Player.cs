600193807416023320c168c0c907106546f71cad
Alexander Lingtorp <Entalpi@users.noreply.github.com>
2019-03-25 17:47:58 +0100

    Touch handling

--- Assets/scripts/Player.cs
+++ Assets/scripts/Player.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class Player : MonoBehaviour {
+public class Player : MonoBehaviour, IEndDragHandler {
 	// Private
 	private const float MOVE_COOLDOWN = 0.25f;
 	private float since_last_move = 0.0f;
@@ -23,7 +24,7 @@ public class Player : MonoBehaviour {
     Vector2Int position = new Vector2Int(tile.boardPosition.x, tile.boardPosition.y);
     board.board[position].Add(gameObject);
 	}
-	
+ 
 	void Update() {
 		since_last_move += Time.deltaTime;
     if (since_last_move <= MOVE_COOLDOWN) { return; }
@@ -57,5 +58,45 @@ public class Player : MonoBehaviour {
 
   private void OnCollisionEnter2D(Collision2D collision) {
     Debug.Log(string.Format("Player hit {0}.", collision.gameObject.name));
+  }
+
+  private enum Direction { Up, Down, Right, Left }
+
+  public void OnEndDrag(PointerEventData eventData) {
+    Vector3 dragVector = (eventData.position - eventData.pressPosition).normalized;
+    bool did_move = false;
+    switch (swipeDirection(dragVector)) {
+      case Direction.Up:
+        animator.Play("Walking Up");
+        did_move = board.validMove(gameObject, tile.boardPosition, Vector3Int.up);
+        break;
+      case Direction.Down;
+        animator.Play("Walking Down");
+        did_move = board.validMove(gameObject, tile.boardPosition, Vector3Int.down);
+        break;
+      case Direction.Left:
+        animator.Play("Walking Left");
+        did_move = board.validMove(gameObject, tile.boardPosition, Vector3Int.left);
+        break;
+      case Direction.Right:
+        animator.Play("Walking Right");
+        did_move = board.validMove(gameObject, tile.boardPosition, Vector3Int.right);
+        break;
+    }
+    if (did_move) {
+      board.endMove();
+      since_last_move = 0.0f;
+      board.CheckGamestate();
+    }
+  }
+
+  private Direction swipeDirection(Vector3 dragVector) {
+    float positiveX = Mathf.Abs(dragVector.x);
+    float positiveY = Mathf.Abs(dragVector.y);
+    if (positiveX > positiveY) {
+      if (dragVector.x > 0) { return Direction.Right } else { return Direction.Left; }
+    } else {
+      if (dragVector.y > 0) { return Direction.Up } else { return Direction.Down; }
+    }
   }
 }
