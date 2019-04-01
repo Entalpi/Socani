0ae7178be23f64ad44fbefc9d3cab479d2e8f765
Alexander Lingtorp <Entalpi@users.noreply.github.com>
2019-04-01 16:06:58 +0200

    Update aseprite-export-layers.bat

--- Assets/levels/aseprite-export-layers.bat
+++ Assets/levels/aseprite-export-layers.bat
@@ -2,7 +2,7 @@ D:
 cd \SteamLibrary\steamapps\common\Aseprite
 :: dir /b /o:gn
 @set ASEPRITE=Aseprite.exe
-@set INPUT_NAME=level7
-@set OUTPUT_NAME=level7
+@set INPUT_NAME=level1
+@set OUTPUT_NAME=%INPUT_NAME%
 %ASEPRITE% -b --split-layers C:\Users\Alexander\repos\Socani\Assets\levels\%INPUT_NAME%\%INPUT_NAME%.aseprite --save-as %OUTPUT_NAME%-{layer}.png
 timeout /t -1
