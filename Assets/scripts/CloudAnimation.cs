047d841454dd7868d38c7f870896a10b45eeb29c
Alexander Lingtorp <Entalpi@users.noreply.github.com>
2019-04-01 14:45:44 +0200

    Cloud animation to playing,scene

--- Assets/scripts/CloudAnimation.cs
+++ Assets/scripts/CloudAnimation.cs
@@ -1,18 +1,21 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
 
-struct Cloud {
+class Cloud {
   public GameObject obj;
   public float speed;
+  public float time;
+  public float waveStrength;
+  public float dt;
 }
 
 public class CloudAnimation : MonoBehaviour {
 
   public List<Texture2D> sprites;
 
-  public int windDirection = -1;
+  public int windDirection = 1;
 
-  public int maxDistance = 100;
+  public float maxDistance = 8.0f;
 
   public int cloudsToSpawn = 8;
 
@@ -24,43 +27,61 @@ public class CloudAnimation : MonoBehaviour {
     } else {
       windDirection = 1;
     }
-    
+
+    windDirection = 1;
+
+
     for (int i = 0; i < cloudsToSpawn; i++) {
       Invoke("spawnCloud", Random.Range(0.5f, 10.0f));
     }
   }
 
   private void Update() {
-    const float waveStrength = 0.02f;
     for (int i = 0; i < clouds.Count; i++) {
-      float x = clouds[i].obj.transform.position.x + clouds[i].speed;
-      float y = clouds[i].obj.transform.position.y + waveStrength * Mathf.Sin(Time.time);
+      clouds[i].time += clouds[i].dt;
+      float x = clouds[i].obj.transform.position.x + clouds[i].speed * windDirection;
+      float y = clouds[i].obj.transform.position.y + clouds[i].waveStrength * Mathf.Sin(clouds[i].time);
       clouds[i].obj.transform.position = new Vector3(x, y, clouds[i].obj.transform.position.z);
-      if (Mathf.Abs(clouds[i].obj.transform.position.x) < maxDistance) {
+      if (Mathf.Abs(clouds[i].obj.transform.position.x) >= maxDistance) {
         reset(clouds[i]);
       }
     }
   }
 
   private void reset(Cloud cloud) {
+    cloud.time = Random.Range(0.0f, 1000.0f);
+    cloud.speed = Random.Range(0.020f, 0.002f);
+    cloud.waveStrength = Random.Range(0.005f, 0.01f);
+    cloud.dt = Random.Range(0.005f, 0.01f);
 
+    float scale = Random.Range(0.25f, 0.70f);
+    cloud.obj.transform.localScale = new Vector3(scale, scale, 1.0f);
+
+    Vector3 randomPosition = new Vector3(maxDistance * -windDirection, 5.0f + Random.Range(-2.0f, 2.0f), 100.0f);
+    cloud.obj.transform.position = randomPosition;
+
+    Texture2D texture = sprites[(int)Random.Range(0.0f, sprites.Count - 1)];
+
+    SpriteRenderer spriteRenderer = cloud.obj.GetComponent<SpriteRenderer>();
+    spriteRenderer.sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), new Vector2(0.0f, 0.0f));
   }
 
   private void spawnCloud() {
     Cloud cloud = new Cloud();
-    cloud.speed = Random.Range(0.05f, 0.2f);
+    cloud.time = Random.Range(0.0f, 1000.0f);
+    cloud.speed = Random.Range(0.020f, 0.002f);
+    cloud.waveStrength = Random.Range(0.005f, 0.01f);
+    cloud.dt = Random.Range(0.005f, 0.01f);
 
     GameObject obj = new GameObject();
     obj.name = "Cloud";
-    float scale = Random.Range(0.75f, 1.0f);
+    float scale = Random.Range(0.25f, 0.70f);
     obj.transform.localScale = new Vector3(scale, scale, 1.0f);
 
-    // obj.transform.position = new Vector3(...);
-    Vector3 randomPosition = new Vector3(5.0f, Random.Range(-4.0f, 4.0f), 100.0f);
+    Vector3 randomPosition = new Vector3(maxDistance * -windDirection, 5.0f + Random.Range(-2.0f, 2.0f), 100.0f);
     obj.transform.position = randomPosition;
 
     Texture2D texture = sprites[(int)Random.Range(0.0f, sprites.Count - 1)];
-
     SpriteRenderer spriteRenderer = obj.AddComponent<SpriteRenderer>() as SpriteRenderer;
     spriteRenderer.sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), new Vector2(0.0f, 0.0f));
 
