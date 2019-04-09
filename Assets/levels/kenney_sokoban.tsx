52bbcb4590be22bc774f9e71b73ab2c799a43a7d
Alexander Lingtorp <Entalpi@users.noreply.github.com>
2019-04-09 15:26:12 +0200

    Added some Tiled maps for testing

---
+++ Assets/levels/kenney_sokoban.tsx
@@ -0,0 +1,232 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.3" name="kenney_sokoban" tilewidth="128" tileheight="128" tilecount="75" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="8">
+  <image width="128" height="128" source="../assets/Environment/environment_01.png"/>
+ </tile>
+ <tile id="9">
+  <image width="128" height="128" source="../assets/Environment/environment_02.png"/>
+ </tile>
+ <tile id="10">
+  <image width="128" height="128" source="../assets/Environment/environment_03.png"/>
+ </tile>
+ <tile id="11">
+  <image width="128" height="128" source="../assets/Environment/environment_04.png"/>
+ </tile>
+ <tile id="12">
+  <image width="128" height="128" source="../assets/Environment/environment_05.png"/>
+ </tile>
+ <tile id="13">
+  <image width="128" height="128" source="../assets/Environment/environment_06.png"/>
+ </tile>
+ <tile id="14">
+  <image width="128" height="128" source="../assets/Environment/environment_07.png"/>
+ </tile>
+ <tile id="15">
+  <image width="128" height="128" source="../assets/Environment/environment_08.png"/>
+ </tile>
+ <tile id="16">
+  <image width="128" height="128" source="../assets/Environment/environment_09.png"/>
+ </tile>
+ <tile id="17">
+  <image width="128" height="128" source="../assets/Environment/environment_10.png"/>
+ </tile>
+ <tile id="18">
+  <image width="128" height="128" source="../assets/Environment/environment_11.png"/>
+ </tile>
+ <tile id="19">
+  <image width="128" height="128" source="../assets/Environment/environment_12.png"/>
+ </tile>
+ <tile id="20">
+  <image width="128" height="128" source="../assets/Environment/environment_13.png"/>
+ </tile>
+ <tile id="21">
+  <image width="128" height="128" source="../assets/Environment/environment_14.png"/>
+ </tile>
+ <tile id="22">
+  <image width="128" height="128" source="../assets/Environment/environment_15.png"/>
+ </tile>
+ <tile id="23">
+  <image width="128" height="128" source="../assets/Environment/environment_16.png"/>
+ </tile>
+ <tile id="24">
+  <image width="128" height="128" source="../assets/Crates/crate_01.png"/>
+ </tile>
+ <tile id="25">
+  <properties>
+   <property name="prefab_name" value="crate"/>
+  </properties>
+  <image width="128" height="128" source="../assets/Crates/crate_02.png"/>
+ </tile>
+ <tile id="26">
+  <image width="128" height="128" source="../assets/Crates/crate_03.png"/>
+ </tile>
+ <tile id="27">
+  <image width="128" height="128" source="../assets/Crates/crate_04.png"/>
+ </tile>
+ <tile id="28">
+  <image width="128" height="128" source="../assets/Crates/crate_05.png"/>
+ </tile>
+ <tile id="29">
+  <image width="128" height="128" source="../assets/Crates/crate_06.png"/>
+ </tile>
+ <tile id="30">
+  <image width="128" height="128" source="../assets/Crates/crate_07.png"/>
+ </tile>
+ <tile id="31">
+  <image width="128" height="128" source="../assets/Crates/crate_08.png"/>
+ </tile>
+ <tile id="32">
+  <image width="128" height="128" source="../assets/Crates/crate_09.png"/>
+ </tile>
+ <tile id="33">
+  <image width="128" height="128" source="../assets/Crates/crate_10.png"/>
+ </tile>
+ <tile id="34">
+  <image width="128" height="128" source="../assets/Crates/crate_11.png"/>
+ </tile>
+ <tile id="35">
+  <image width="128" height="128" source="../assets/Crates/crate_12.png"/>
+ </tile>
+ <tile id="36">
+  <image width="128" height="128" source="../assets/Crates/crate_13.png"/>
+ </tile>
+ <tile id="37">
+  <image width="128" height="128" source="../assets/Crates/crate_14.png"/>
+ </tile>
+ <tile id="38">
+  <image width="128" height="128" source="../assets/Crates/crate_15.png"/>
+ </tile>
+ <tile id="39">
+  <image width="128" height="128" source="../assets/Crates/crate_16.png"/>
+ </tile>
+ <tile id="40">
+  <image width="128" height="128" source="../assets/Crates/crate_17.png"/>
+ </tile>
+ <tile id="41">
+  <image width="128" height="128" source="../assets/Crates/crate_18.png"/>
+ </tile>
+ <tile id="42">
+  <image width="128" height="128" source="../assets/Crates/crate_19.png"/>
+ </tile>
+ <tile id="43">
+  <image width="128" height="128" source="../assets/Crates/crate_20.png"/>
+ </tile>
+ <tile id="44">
+  <image width="128" height="128" source="../assets/Crates/crate_21.png"/>
+ </tile>
+ <tile id="45">
+  <image width="128" height="128" source="../assets/Crates/crate_22.png"/>
+ </tile>
+ <tile id="46">
+  <image width="128" height="128" source="../assets/Crates/crate_23.png"/>
+ </tile>
+ <tile id="47">
+  <image width="128" height="128" source="../assets/Crates/crate_24.png"/>
+ </tile>
+ <tile id="48">
+  <image width="128" height="128" source="../assets/Crates/crate_25.png"/>
+ </tile>
+ <tile id="49">
+  <image width="128" height="128" source="../assets/Crates/crate_26.png"/>
+ </tile>
+ <tile id="50">
+  <image width="128" height="128" source="../assets/Crates/crate_27.png"/>
+ </tile>
+ <tile id="51">
+  <image width="128" height="128" source="../assets/Crates/crate_28.png"/>
+ </tile>
+ <tile id="52">
+  <image width="128" height="128" source="../assets/Crates/crate_29.png"/>
+ </tile>
+ <tile id="53">
+  <image width="128" height="128" source="../assets/Crates/crate_30.png"/>
+ </tile>
+ <tile id="54">
+  <image width="128" height="128" source="../assets/Crates/crate_31.png"/>
+ </tile>
+ <tile id="55">
+  <image width="128" height="128" source="../assets/Crates/crate_32.png"/>
+ </tile>
+ <tile id="56">
+  <image width="128" height="128" source="../assets/Crates/crate_33.png"/>
+ </tile>
+ <tile id="57">
+  <image width="128" height="128" source="../assets/Crates/crate_34.png"/>
+ </tile>
+ <tile id="58">
+  <image width="128" height="128" source="../assets/Crates/crate_35.png"/>
+ </tile>
+ <tile id="59">
+  <image width="128" height="128" source="../assets/Crates/crate_36.png"/>
+ </tile>
+ <tile id="60">
+  <image width="128" height="128" source="../assets/Crates/crate_37.png"/>
+ </tile>
+ <tile id="61">
+  <image width="128" height="128" source="../assets/Crates/crate_38.png"/>
+ </tile>
+ <tile id="62">
+  <image width="128" height="128" source="../assets/Crates/crate_39.png"/>
+ </tile>
+ <tile id="63">
+  <image width="128" height="128" source="../assets/Crates/crate_40.png"/>
+ </tile>
+ <tile id="64">
+  <image width="128" height="128" source="../assets/Crates/crate_41.png"/>
+ </tile>
+ <tile id="65">
+  <image width="128" height="128" source="../assets/Crates/crate_42.png"/>
+ </tile>
+ <tile id="66">
+  <image width="128" height="128" source="../assets/Crates/crate_43.png"/>
+ </tile>
+ <tile id="67">
+  <image width="128" height="128" source="../assets/Crates/crate_44.png"/>
+ </tile>
+ <tile id="68">
+  <image width="128" height="128" source="../assets/Crates/crate_45.png"/>
+ </tile>
+ <tile id="69">
+  <image width="128" height="128" source="../assets/Blocks/block_01.png"/>
+ </tile>
+ <tile id="70">
+  <image width="128" height="128" source="../assets/Blocks/block_02.png"/>
+ </tile>
+ <tile id="71">
+  <image width="128" height="128" source="../assets/Blocks/block_03.png"/>
+ </tile>
+ <tile id="72">
+  <image width="128" height="128" source="../assets/Blocks/block_04.png"/>
+ </tile>
+ <tile id="73">
+  <image width="128" height="128" source="../assets/Blocks/block_05.png"/>
+ </tile>
+ <tile id="74">
+  <image width="128" height="128" source="../assets/Blocks/block_06.png"/>
+ </tile>
+ <tile id="75">
+  <image width="128" height="128" source="../assets/Blocks/block_07.png"/>
+ </tile>
+ <tile id="76">
+  <image width="128" height="128" source="../assets/Blocks/block_08.png"/>
+ </tile>
+ <tile id="77">
+  <image width="128" height="128" source="../assets/Ground/ground_01.png"/>
+ </tile>
+ <tile id="78">
+  <image width="128" height="128" source="../assets/Ground/ground_02.png"/>
+ </tile>
+ <tile id="79">
+  <image width="128" height="128" source="../assets/Ground/ground_03.png"/>
+ </tile>
+ <tile id="80">
+  <image width="128" height="128" source="../assets/Ground/ground_04.png"/>
+ </tile>
+ <tile id="81">
+  <image width="128" height="128" source="../assets/Ground/ground_05.png"/>
+ </tile>
+ <tile id="82">
+  <image width="128" height="128" source="../assets/Ground/ground_06.png"/>
+ </tile>
+</tileset>
