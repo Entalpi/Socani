41bcf98ed2182a0ff7a4b06e0f7326e1db6376cf
Alexander Lingtorp <Entalpi@users.noreply.github.com>
2019-04-23 15:03:00 +0200

    Update Crate.cs

--- Assets/Resources/scripts/Crate.cs
+++ Assets/Resources/scripts/Crate.cs
@@ -3,5 +3,31 @@
 public enum CrateColor { blue, red, green, normal }
 
 public class Crate : MonoBehaviour {
-  public CrateColor crateColor;
+
+  public Sprite redSprite;
+  public Sprite blueSprite;
+  public Sprite greenSprite;
+  public Sprite normalSprite;
+
+  public CrateColor _Color;  // 'backing field' ...
+  public CrateColor Color {
+    set {
+      switch (value) {
+        case CrateColor.red:
+          GetComponent<SpriteRenderer>().sprite = redSprite;
+          break;
+        case CrateColor.blue:
+          GetComponent<SpriteRenderer>().sprite = blueSprite;
+          break;
+        case CrateColor.green:
+          GetComponent<SpriteRenderer>().sprite = greenSprite;
+          break;
+        case CrateColor.normal:
+          GetComponent<SpriteRenderer>().sprite = normalSprite;
+          break;
+      }
+      _Color = value;
+    }
+    get { return _Color; }
+  }
 }
